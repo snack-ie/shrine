a0f4494748af5d145d409a4d60102e8caa8a1b5d
GUIMED <90587015+Guiguimed@users.noreply.github.com>
2021-12-05 17:26:30 -0300

    shrines

---
+++ data.js
@@ -0,0 +1,32 @@
+let data = [{
+	title: "Shiki Shrine",
+	description: "Ok, isso aqui é um site para adorarmos a Shiki, começe a adorar ela imediatamente",
+	video: "https://www.youtube.com/embed/cVpU3ckQyJg",
+	image: "shiki.png",
+	alt: "Shiki",
+	color: "LightBlue"
+},
+{
+	title: "Rhyme Shrine",
+	description: "Ok, isso aqui é um site para adorarmos a Rhyme, começe a adorar ela imediatamente",
+	video: "https://www.youtube.com/embed/ibIWCYqj8rc",
+	image: "rhyme.png",
+	alt: "Rhyme",
+	color: "Orange"
+},
+{
+    title: "Joshua Shrine",
+    description: "Ok, isso aqui é um site para adorarmos o Joshua, começe a adorar ele imediatamente",
+    video: "https://www.youtube.com/embed/mgBQKoq3rEg",
+    image: "joshua.png",
+    alt: "Joshua",
+    color: "LightCyan"
+},
+{
+    title: "Anya Shrine",
+    description: "Ok, isso aqui é um site para adorarmos a Anya, começe a adorar ela imediatamente",
+    video: "https://www.youtube.com/embed/tnhoGZMv1oM",
+    image: "anya.png",
+    alt: "Anya",
+    color: "LightPink"
+}];
