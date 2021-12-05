64b945c144a035a52d39c6cb418110bd2ec6e020
GUIMED <90587015+Guiguimed@users.noreply.github.com>
2021-12-05 16:25:43 -0300

    shiki

---
+++ shiki.js
@@ -0,0 +1,10 @@
+
+function shiki() {
+	document.getElementById("title").innerHTML = "Shiki Shrine"
+	document.getElementById("p").innerHTML = "Ok, isso aqui é um site para adorarmos a Shiki, começe a adorar ela imediatamente"
+	document.getElementById("linl").src = "https://www.youtube.com/embed/cVpU3ckQyJg"
+	document.getElementById("foto").src = "shiki.png"
+	document.getElementById("foto").alt = "shiki"
+	document.getElementById("title").style.color = "LightBlue"
+	document.getElementById("p").style.color = "LightBlue"
+}	
